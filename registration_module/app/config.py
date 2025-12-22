b7d1b7089e0bab14f967daf56bdfb690a157eaca
SrujKashyap <Srujankashyap21@gmail.com>
2025-12-22 22:28:05 +0530

    changed config for production

--- registration_module/app/config.py
+++ registration_module/app/config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 class BaseConfig:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-in-production")
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "dev-secret-key-change-in-production")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     #jwt settings
@@ -10,9 +10,9 @@ class BaseConfig:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=int(os.environ.get("JWT_ACCESS_MINUTES", 15)))
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=int(os.environ.get("JWT_REFRESH_DAYS", 30))) 
     JWT_TOKEN_LOCATION = ["headers", "cookies"]
-    JWT_COOKIE_SECURE = False #set to true in Production
-    JWT_COOKIE_SAMESITE = "Lax"  # Use Lax for localhost development
-    JWT_COOKIE_CSRF_PROTECT = False  # Disable for development
+    JWT_COOKIE_SECURE = True #set to true in Production and Flase in dev
+    JWT_COOKIE_SAMESITE = "None"  # Use Lax for localhost development
+    JWT_COOKIE_CSRF_PROTECT = True  # Disable for development
     JWT_COOKIE_DOMAIN = None  # Allow cookies on localhost
     JWT_ACCESS_COOKIE_PATH = "/"
     JWT_REFRESH_COOKIE_PATH = "/" 
@@ -24,7 +24,7 @@ class DevelopmentConfig(BaseConfig):
      SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL", "sqlite:///app.db")
 
 
-class ProductionConfig:
+class ProductionConfig(BaseConfig):
      DEBUG = False
      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
      
